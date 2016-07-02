fb8ec5682d42fe9946f0c6ed868ae43cff2f31e6
Kenneth Crawford <kmc059000@gmail.com>
2016-07-02 11:41:02 -0500

    utility for declaring mappings between two enums

---
+++ src/EnumMapper/EnumMapper.Tests/EnumMapperTests.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EnumMapper.Tests
+{
+    [TestClass]
+    public class EnumMapperTests
+    {
+        [TestMethod]
+        public void EnumMapper_MapsEnums()
+        {
+            var enumMapper = new E1E2EnumMapper();
+
+            Assert.AreEqual(E2.A, enumMapper.Map(E1.A));
+            Assert.AreEqual(E2.B, enumMapper.Map(E1.B));
+            Assert.AreEqual(E2.C, enumMapper.Map(E1.C));
+            Assert.AreEqual(E2.D, enumMapper.Map(E1.D));
+
+            Assert.AreEqual(E1.A, enumMapper.Map(E2.A));
+            Assert.AreEqual(E1.B, enumMapper.Map(E2.B));
+            Assert.AreEqual(E1.C, enumMapper.Map(E2.C));
+            Assert.AreEqual(E1.D, enumMapper.Map(E2.D));
+        }
+
+        [TestMethod]
+        public void EnumMapperAutoMap_DoesNotMapEnums_IfMappingDoesntExist_MapsEnums()
+        {
+            var enumMapper = new E1E2EnumMapper();
+
+            ThrowsException(() => enumMapper.Map(E1.E));
+            ThrowsException(() => enumMapper.Map(E1.F));
+            ThrowsException(() => enumMapper.Map(E1.G));
+            ThrowsException(() => enumMapper.Map(E1.H));
+
+            ThrowsException(() => enumMapper.Map(E2.X));
+            ThrowsException(() => enumMapper.Map(E2.Y));
+            ThrowsException(() => enumMapper.Map(E2.Z));
+
+            Assert.AreEqual(null, enumMapper.TryMap((E1?)null));
+            Assert.AreEqual(null, enumMapper.TryMap(E1.E));
+            Assert.AreEqual(null, enumMapper.TryMap(E1.F));
+            Assert.AreEqual(null, enumMapper.TryMap(E1.G));
+            Assert.AreEqual(null, enumMapper.TryMap(E1.H));
+
+            Assert.AreEqual(null, enumMapper.TryMap((E2?)null));
+            Assert.AreEqual(null, enumMapper.TryMap(E2.X));
+            Assert.AreEqual(null, enumMapper.TryMap(E2.Y));
+            Assert.AreEqual(null, enumMapper.TryMap(E2.Z));
+
+        }
+
+        [TestMethod]
+        public void EnumMapperAutoMap_MapsEnums()
+        {
+            var enumMapper = new E1E3EnumAutoMapper();
+
+            Assert.AreEqual(E3.E, enumMapper.Map(E1.E));
+            Assert.AreEqual(E3.F, enumMapper.Map(E1.F));
+            Assert.AreEqual(E3.G, enumMapper.Map(E1.G));
+            Assert.AreEqual(E3.H, enumMapper.Map(E1.H));
+
+            Assert.AreEqual(E1.E, enumMapper.Map(E3.E));
+            Assert.AreEqual(E1.F, enumMapper.Map(E3.F));
+            Assert.AreEqual(E1.G, enumMapper.Map(E3.G));
+            Assert.AreEqual(E1.H, enumMapper.Map(E3.H));
+        }
+
+        [TestMethod]
+        public void EnumMapper_DoesNotMapEnums_IfMappingDoesntExist_MapsEnums()
+        {
+            var enumMapper = new E1E3EnumAutoMapper();
+
+            ThrowsException(() => enumMapper.Map(E1.A));
+            ThrowsException(() => enumMapper.Map(E1.B));
+            ThrowsException(() => enumMapper.Map(E1.C));
+            ThrowsException(() => enumMapper.Map(E1.D));
+
+            ThrowsException(() => enumMapper.Map(E3.X));
+            ThrowsException(() => enumMapper.Map(E3.Y));
+            ThrowsException(() => enumMapper.Map(E3.Z));
+
+            Assert.AreEqual(null, enumMapper.TryMap((E1?)null));
+            Assert.AreEqual(null, enumMapper.TryMap(E1.A));
+            Assert.AreEqual(null, enumMapper.TryMap(E1.B));
+            Assert.AreEqual(null, enumMapper.TryMap(E1.C));
+            Assert.AreEqual(null, enumMapper.TryMap(E1.D));
+
+            Assert.AreEqual(null, enumMapper.TryMap((E3?)null));
+            Assert.AreEqual(null, enumMapper.TryMap(E3.X));
+            Assert.AreEqual(null, enumMapper.TryMap(E3.Y));
+            Assert.AreEqual(null, enumMapper.TryMap(E3.Z));
+        }
+
+        private void ThrowsException<T>(Func<T> func)
+        {
+            var thrown = false;
+            try
+            {
+                func();
+            }
+            catch
+            {
+                thrown = true;
+            }
+
+            if (!thrown)
+            {
+                Assert.Fail("Exception was not thrown");
+            }
+        }
+
+        public class E1E2EnumMapper : EnumMapper<E1, E2>
+        {
+            static E1E2EnumMapper()
+            {
+                CreateMap(E1.A, E2.A);
+                CreateMap(E1.B, E2.B);
+                CreateMap(E1.C, E2.C);
+                CreateMap(E1.D, E2.D);
+            }
+        }
+
+        public class E1E3EnumAutoMapper : EnumMapper<E1, E3>
+        {
+            static E1E3EnumAutoMapper()
+            {
+                AutoMap();
+            }
+        }
+
+
+        public enum E1
+        {
+            A,
+            B,
+            C,
+            D,
+            E,
+            F,
+            G,
+            H
+        }
+
+        public enum E2
+        {
+            A,
+            B,
+            C,
+            D,
+
+            X,
+            Y,
+            Z
+        }
+
+        public enum E3
+        {
+            E,
+            F,
+            G,
+            H,
+
+            X,
+            Y,
+            Z
+        }
+
+    }
+}
---
+++ src/EnumMapper/EnumMapper/EnumMapper.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EnumMapper
+{
+    public abstract class EnumMapper<TEnum1, TEnum2> 
+        where TEnum1 : struct 
+        where TEnum2 : struct 
+    {
+        private static readonly IDictionary<TEnum1, TEnum2> Map1 = new Dictionary<TEnum1, TEnum2>(); 
+        private static readonly IDictionary<TEnum2, TEnum1> Map2 = new Dictionary<TEnum2, TEnum1>();
+
+        protected static void AutoMap()
+        {
+            var values1 = Enum.GetValues(typeof (TEnum1)).Cast<TEnum1>().ToList();
+            var values2 = Enum.GetValues(typeof (TEnum2)).Cast<TEnum2>().ToList();
+
+            var matches =
+                from a in values1
+                from b in values2
+                where a.ToString() == b.ToString()
+                select new {a, b};
+
+            foreach (var match in matches)
+            {
+                CreateMap(match.a, match.b);
+            }
+        }
+
+        protected static void CreateMap(TEnum1 e1, TEnum2 e2)
+        {
+            Map1.Add(e1, e2);
+            Map2.Add(e2, e1);
+        }
+
+        public TEnum2 Map(TEnum1 source)
+        {
+            return Map1[source];
+        }
+
+        public TEnum1 Map(TEnum2 source)
+        {
+            return Map2[source];
+        }
+
+        public TEnum2? TryMap(TEnum1? source)
+        {
+            return source.HasValue
+                ? Map1.ContainsKey(source.Value)
+                    ? Map1[source.Value]
+                    : (TEnum2?) null
+                : null;
+        }
+
+        public TEnum1? TryMap(TEnum2? source)
+        {
+            return source.HasValue
+                ? Map2.ContainsKey(source.Value)
+                    ? Map2[source.Value]
+                    : (TEnum1?) null
+                : null;
+        }
+    }
+}
